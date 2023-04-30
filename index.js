2d23c4b4645eb2fd63ded291ed56bd75a488ecd9
Aakash Gajjar <102506089+akash-gajjar@users.noreply.github.com>
2023-04-30 09:10:32 +0530

    feat(root): add benchmarking inf

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cron = require('node-cron');
+
+cron.schedule('* * * * * *', () => {
+    const a = [];
+    for (let i =0; i<10000; i++){
+        for (let j=0; j<10000; j++){
+            a.push(i*j);
+        }
+    }
+    console.log('running a task every minute');
+});
+
+const app = express();
+
+app.get('/', (req, res) => res.send('hi'));
+
+app.listen(1234, () => console.log(`listening`));
